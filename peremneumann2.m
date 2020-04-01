c9aa6ae0862764099bd6f857e195f2e5cb65761a
Imre Fekete <feipaat@cs.elte.hu>
2020-04-01 14:43:06 +0200

    Add files via upload

---
+++ peremneumann2.m
@@ -0,0 +1,24 @@
+function [h,y]=peremneumann2(N)
+
+a=0; b=1;
+alpha=1; beta=exp(1);
+
+%%Racs
+h = (b-a)/(N+1);
+
+%%A_h_hullam
+e = ones(N+2,1);
+A_h_hullam=(1/h^2)*spdiags([e -2*e e],[-1,0,1],N+2,N+2);
+% Neumann perem (masodik)
+A_h_hullam(1,1)=-1/h;
+A_h_hullam(1,2)=1/h;
+% Dirichlet perem
+A_h_hullam(N+2,N+1)=0;
+A_h_hullam(N+2,N+2)=1;
+
+%% f_hullam (itt tortenik a valtozas)
+x = linspace(a,b,N+2)';
+f = exp(x); f(1)=alpha+(h/2)*exp(a); f(N+2)=beta;
+
+%% LAER mo-sa
+y=A_h_hullam\f;
